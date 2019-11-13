758ee62c7ab4251410c5777fcd1327c84b22d20a
GG <920507033@qq.com>
2019-11-13 20:14:37 +0800

    updatecommit

--- git_githup/src/main/java/com/it/pojo/User.java
+++ git_githup/src/main/java/com/it/pojo/User.java
@@ -1,15 +1,25 @@
 package com.it.pojo;
 
 public class User {
+    private Integer id;
     private String name;
     private Integer age;
 
     @Override
     public String toString() {
         return "User{" +
-                "name='" + name + '\'' +
+                "id=" + id +
+                ", name='" + name + '\'' +
                 ", age=" + age +
                 '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getName() {
